947c6c7c2711bde2a966b5b3cc82756aef5b7b41
Chris Mungall <cjm@berkeleybop.org>
2014-07-19 17:01:46 -0700

    pack

--- pack.pl
+++ pack.pl
@@ -1,3 +1,7 @@
 name( rdfs2pl ).
 title( 'compile an RDFS schema to prolog predicates' ).
+keywords([semweb,rdf,schema,ontology]).
 version( '0.0.1' ).
+author('Chris Mungall','cmungall@gmail.com').
+home('https://github.com/cmungall/rdfs2pl').
+download('https://github.com/cmungall/rdfs2pl.git').
